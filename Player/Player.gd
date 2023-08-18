3a5ff6d77ff2a34bafdf68a61f5cabcdc879e1e5
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-08-18 13:27:35 -0300

    Atualização do script de movimento do personagem

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
-@export var speed = 100
+var speed = 100
+const acceleration = 10
+const friction = 10
 
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_direction * speed
+	if input_direction != Vector2.ZERO:
+		velocity += input_direction * acceleration
+		velocity = velocity.limit_length(speed)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction)
 
 func _physics_process(_delta):
 	get_input()
