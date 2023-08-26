c9c0bb8dbf51982ac40b2cdcb20ce2c2d36c6234
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-08-26 18:44:56 -0300

    Ligação das animações a se o personagem está se movendo ou não

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,12 +4,19 @@ var speed = 100
 const acceleration = 10
 const friction = 10
 
+@onready var animation_player = $AnimationPlayer
+
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if input_direction != Vector2.ZERO:
+		if input_direction.x > 0:
+			animation_player.play("RunRight")
+		else:
+			animation_player.play("RunLeft")
 		velocity += input_direction * acceleration
 		velocity = velocity.limit_length(speed)
 	else:
+		animation_player.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, friction)
 
 func _physics_process(_delta):
