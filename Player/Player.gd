0a24dce6ac755cf62736f2b9bb3bd7e70a67d308
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-08-27 13:31:35 -0300

    Animações de movimento com AnimationTree

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,18 +5,19 @@ const acceleration = 10
 const friction = 10
 
 @onready var animation_player = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var animation_state = animation_tree.get("parameters/playback")
 
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if input_direction != Vector2.ZERO:
-		if input_direction.x > 0:
-			animation_player.play("RunRight")
-		else:
-			animation_player.play("RunLeft")
+		animation_tree.set("parameters/Idle/blend_position", input_direction)
+		animation_tree.set("parameters/Run/blend_position", input_direction)
+		animation_state.travel("Run")
 		velocity += input_direction * acceleration
 		velocity = velocity.limit_length(speed)
 	else:
-		animation_player.play("IdleRight")
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction)
 
 func _physics_process(_delta):
