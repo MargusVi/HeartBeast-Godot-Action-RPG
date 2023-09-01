62ee208eddcbc7887a4421039c3e11806df1ffb6
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-08-31 21:20:47 -0300

    Adicionadas animações de ataque e State Machines

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,25 +1,52 @@
 extends CharacterBody2D
 
-var speed = 100
+const speed = 100
 const acceleration = 10
 const friction = 10
+
+enum{
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 
 @onready var animation_player = $AnimationPlayer
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
 
-func get_input():
+func _ready():
+	animation_tree.active = true
+
+func _physics_process(_delta):
+	match state:
+		MOVE:
+			move_state()
+		ROLL:
+			pass
+		ATTACK:
+			attack_state()
+
+func move_state():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if input_direction != Vector2.ZERO:
 		animation_tree.set("parameters/Idle/blend_position", input_direction)
 		animation_tree.set("parameters/Run/blend_position", input_direction)
+		animation_tree.set("parameters/Attack/blend_position", input_direction)
 		animation_state.travel("Run")
 		velocity += input_direction * acceleration
 		velocity = velocity.limit_length(speed)
 	else:
 		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction)
-
-func _physics_process(_delta):
-	get_input()
 	move_and_slide()
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	
+func attack_state():
+	velocity = Vector2.ZERO
+	animation_state.travel("Attack")
+	
+func attack_animation_finished():
+	state = MOVE
